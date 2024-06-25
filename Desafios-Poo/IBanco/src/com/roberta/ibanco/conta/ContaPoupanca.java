b832dc7657a03d0c7c85b9622d5f6e3c45a20e19
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-06-24 20:34:29 -0400

    pack

--- Desafios-Poo/IBanco/src/com/roberta/ibanco/conta/ContaPoupanca.java
+++ Desafios-Poo/IBanco/src/com/roberta/ibanco/conta/ContaPoupanca.java
@@ -7,7 +7,7 @@ public class ContaPoupanca extends Conta {
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupan�a ===");
+		System.out.println("=== Extrato Conta Poupanca ===");
 		super.imprimirInfosComuns();
 	}
 }
